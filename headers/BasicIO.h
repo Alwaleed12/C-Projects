9e8b06a45b1b6f56fcb0cef256dc73d8feb11177
Alwaleed Algheraibi <alwaleedalgheraibi@Alwaleeds-MacBook-Pro.local>
2022-10-06 11:09:23 -0400

    Added new files named BasicIO and Descisions

---
+++ headers/BasicIO.h
@@ -0,0 +1,10 @@
+// include pre-processor directives
+#define FIRST_NAME "Alwaleed"
+#define LAST_NAME "Algheraibi"
+#define WEIGHT 115.5
+#define AGE 25
+#include <stdio.h>
+
+// include function declarations
+void output(void);
+void input(void);
