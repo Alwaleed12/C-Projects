5bbc12ecf343bc98f8e3022bf83910b2756ad7d7
Alwaleed Algheraibi <alwaleedalgheraibi@alwaleeds-mbp.mynetworksettings.com>
2022-10-17 21:37:14 -0400

    Added Functions functionality

--- App.c
+++ App.c
@@ -4,6 +4,7 @@
 #include "./headers/BasicIO.h"
 #include "./headers/Decisions.h"
 #include "./headers/Loops.h"
+#include "./headers/Functions.h"
 
 int main (void){
     // puts("Hello World!!!");
@@ -18,9 +19,18 @@ int main (void){
     // decisions5();
     // decisions6();
     // testing Loops functionality
-    loops1();
-    loops2();
-    loops3();
-    loops4();
-    loops5();
+    // loops1();
+    // loops2();
+    // loops3();
+    // loops4();
+    // loops5();
+    // testing Functions functionality
+    // declare three character arrays and initialize them
+    char student[] = "Alwaleed";
+    char course[] = "Ruby";
+    char programmer[] = "Yikihiro Mastumoto";
+    congratulate1(student, course, programmer);
+    congratulate2(student, course, programmer);
+    printf("%.2f degree celsius = %.2f degrees fahrenheit\n", 100.0, 
+        fahrenheit(100.0));
 }
